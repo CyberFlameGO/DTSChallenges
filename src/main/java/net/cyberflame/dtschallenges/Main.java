85fab82e3d84f63e6391d9d5e6b9e0337aea1686
CyberFlame <cyberflameu@gmail.com>
2023-02-08 13:48:36 +1300

    and that's a wrap folks

--- src/main/java/net/cyberflame/dtschallenges/Main.java
+++ src/main/java/net/cyberflame/dtschallenges/Main.java
@@ -1,14 +1,9 @@
 package net.cyberflame.dtschallenges;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
-import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.ResourceBundle;
 
 public enum Main {
     ;
@@ -20,34 +15,24 @@ public enum Main {
     public static void main(final String[] args) {
         final Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
         final int gsm;
-        final ResourceBundle resourceBundle = new ResourceBundle() {
-            @Override
-            protected @Nullable Object handleGetObject(@NotNull String key) {
-                return null;
-            }
-
-            @Override
-            public @Nullable Enumeration<String> getKeys() {
-                return null;
-            }
-        };
-        final String paperTypeQuestion = resourceBundle.getString("paper.type");
+        final String paperTypeQuestion = "Poster paper, or office paper? ";
         System.out.print(paperTypeQuestion);
         gsm = switch (scanner.nextLine().toLowerCase(Locale.ROOT).strip()) {
             case "poster" -> 130;
             case "office" -> 90;
-            case default -> throw new RuntimeException("Invalid paper type");
+            default ->
+                    throw new IllegalStateException("Unexpected value, try 'poster' or 'office'.");
         };
-        final String pagesAmountQuestion = resourceBundle.getString("pages.amount");
+        final String pagesAmountQuestion = "How many pages is your booklet: ";
         System.out.print(pagesAmountQuestion);
         try {
             final int number = scanner.nextInt();
             final Integer calculatedWeight = Integer.valueOf(number * gsm);
-            final String rawBookletStr = resourceBundle.getString("booklet.weight");
+            final String rawBookletStr = "The booklet weight is: {0}gsm";
             final String bookletWeightFString = MessageFormat.format(rawBookletStr, calculatedWeight);
             System.out.println(bookletWeightFString);
         } catch (final RuntimeException e) {
-            final String invalidPagesError = resourceBundle.getString("invalid.pages");
+            final String invalidPagesError = "Invalid number of pages!";
             System.out.println(invalidPagesError);
         } finally {
             scanner.close();
