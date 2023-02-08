75d2990dd5755cca14f5112ef94a70bb06cb4b49
CyberFlame <cyberflameu@gmail.com>
2023-02-09 09:13:55 +1300

    move all strings to top

--- src/main/java/net/cyberflame/dtschallenges/Main.java
+++ src/main/java/net/cyberflame/dtschallenges/Main.java
@@ -16,6 +16,10 @@ public enum Main {
         final Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
         final int gsm;
         final String paperTypeQuestion = "Poster paper, or office paper? ";
+        final String rawBookletStr = "The booklet weight is: {0}gsm";
+        final String invalidPagesError = "Invalid number of pages!";
+        final String pagesAmountQuestion = "How many pages is your booklet: ";
+        
         System.out.print(paperTypeQuestion);
         gsm = switch (scanner.nextLine().toLowerCase(Locale.ROOT).strip()) {
             case "poster" -> 130;
@@ -23,16 +27,13 @@ public enum Main {
             default ->
                     throw new IllegalStateException("Unexpected value, try 'poster' or 'office'.");
         };
-        final String pagesAmountQuestion = "How many pages is your booklet: ";
         System.out.print(pagesAmountQuestion);
         try {
             final int number = scanner.nextInt();
             final Integer calculatedWeight = Integer.valueOf(number * gsm);
-            final String rawBookletStr = "The booklet weight is: {0}gsm";
             final String bookletWeightFString = MessageFormat.format(rawBookletStr, calculatedWeight);
             System.out.println(bookletWeightFString);
         } catch (final RuntimeException e) {
-            final String invalidPagesError = "Invalid number of pages!";
             System.out.println(invalidPagesError);
         } finally {
             scanner.close();
