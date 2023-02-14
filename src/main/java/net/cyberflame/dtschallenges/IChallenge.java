36cf614ed35f7af7e4ed7e28159df99352087cd5
CyberFlame <cyberflameu@gmail.com>
2023-02-15 10:56:26 +1300

    small changes

--- src/main/java/net/cyberflame/dtschallenges/IChallenge.java
+++ src/main/java/net/cyberflame/dtschallenges/IChallenge.java
@@ -1,8 +1,10 @@
 package net.cyberflame.dtschallenges;
 
+import java.io.IOException;
+
 public interface IChallenge {
 
-    void main(String[] args);
+    void main(String[] args) throws IOException;
     default void run() {
         System.out.println("Running challenge " + this.getClass().getSimpleName());
         main(new String[] {});
--- src/main/java/net/cyberflame/dtschallenges/challenges/Challenge02.java
+++ src/main/java/net/cyberflame/dtschallenges/challenges/Challenge02.java
@@ -2,9 +2,12 @@ package net.cyberflame.dtschallenges.challenges;
 
 import net.cyberflame.dtschallenges.IChallenge;
 
+import java.io.IOException;
+
 public class Challenge02 implements IChallenge {
     @Override
-    public void main(final String[] args) {
+    public void main(final String[] args) throws IOException {
+
 
     }
 }
