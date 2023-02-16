bc65d9192a9b1262f1e48dbae23601d2dc7f091a
CyberFlame <cyberflameu@gmail.com>
2023-02-16 13:55:00 +1300

    comment + test

--- src/main/resources/__main__.py
+++ src/main/resources/__main__.py
@@ -1,7 +1,13 @@
-import zipfile, os, argparse, csv
+import zipfile, os, argparse, csv, sys
 
 
 print(__name__)
+
+print(sys.argv[1], sys.argv[2])
+
+#sys.argv[1] needs to match os.getpgid(0) just to mitigate people trying from trying to run the
+#script from the command line (it's just a small check for pids, not a security measure)
+# but we should use argparse not argv
 
 print(os.getpid.__name__, os.getpid())
 print(os.getpgid.__name__, os.getpgid(0))
